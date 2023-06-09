03872cce78d82b6b99359496e5183f15db366d67
Piyush Thapa <112943430+Herewego2004@users.noreply.github.com>
2023-06-09 15:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const timing = document.getElementById("time");
+const alarmSound = new Audio("alarmSound.mp3");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const setbutton = document.getElementById("setalarm");
+const dropdowns = document.getElementsByClassName('dropdowns');
+
+
+
+setInterval(() => {
+    let time = new Date();
+    let hr = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    if (hr < 10) {
+        if (min < 10) {
+            if (sec < 10) {
+                timing.innerText = `Current Time: 0${hr}:0${min}:0${sec}`;
+            }
+            else {
+                timing.innerText = `Current Time: 0${hr}:0${min}:${sec}`;
+            }
+        } else {
+            if (sec < 10) {
+                timing.innerText = `Current Time: 0${hr}:${min}:0${sec}`;
+            } else {
+                timing.innerText = `Current Time: 0${hr}:${min}:${sec}`;
+            }
+        }
+    } else {
+        if (min < 10) {
+            if (sec < 10) {
+                timing.innerText = `Current Time: ${hr}:0${min}:0${sec}`;
+            }
+            else {
+                timing.innerText = `Current Time: ${hr}:0${min}:${sec}`;
+            }
+        } else {
+            if (sec < 10) {
+                timing.innerText = `Current Time: ${hr}:${min}:0${sec}`;
+            } else {
+                timing.innerText = `Current Time: ${hr}:${min}:${sec}`;
+            }
+        }
+    }
+}, 1000);
+setbutton.addEventListener("click", () => {
+    if (
+        hour.value === "Hour" ||
+        minute.value === "Minute" ||
+        second.value === "Second"
+    ) {
+        alert("Invalid Time set.");
+    } else {
+        let settime = `${hour.value}:${minute.value}:${second.value}`;
+        setInterval(() => {
+            if (timing.innerText == settime) {
+                alarmSound.play();
+            }  
+        }, 20)
+    }
+});
+
+
+        function reset(){
+            location.reload(); 
+        }
+        
+        //   let time = new Date();
+        //   let hr = time.getHours();
+        //   let min = time.getMinutes();
+        //   let sec = time.getSeconds();
+        //     setTimeout(() => {
+        //       alarmSound.play();
+        //     }, ((hour.value - hr) * 3600 + (minute.value - min) * 60 + (second.value - sec)) 
+        // * 1000);
